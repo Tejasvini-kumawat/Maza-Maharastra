421d53099fd1aa6151ab59d141bbc2cd25d1aaaf
Tejasvini Kumawat <tejasvinikumawat2004@gmail.com>
2024-07-25 08:57:23 +0530

    Add all Files

---
+++ history.js
@@ -0,0 +1,8 @@
+let btn1=document.querySelector('.btn-1');
+btn1.addEventListener("click",()=>{
+document.querySelector('.maha').src="https://us.123rf.com/450wm/infinitegraphic/infinitegraphic1804/infinitegraphic180400625/99824015-illustration-of-india-map-showing-indian-state-maharashtra-with-hindi-text-jai-maharashtra-meaning.jpg?ver=6";
+});
+let btn2=document.querySelector('.btn-2');
+btn2.addEventListener("click",()=>{
+document.querySelector('.maha').src="https://www.whereig.com/india/states/map/maharashtra-district-map.jpg";
+});
